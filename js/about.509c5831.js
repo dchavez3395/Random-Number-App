d221dd4e9b9ef2cd50fa462f5f3c6065d8adbbdf
Daniel <danilias666-@hotmail.com>
2022-07-10 12:46:18 -0500

    first commit

---
+++ js/about.509c5831.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunklearn_vuex"]=self["webpackChunklearn_vuex"]||[]).push([[443],{7381:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var a=u(3396);const r={class:"about"},t=(0,a._)("h1",null,"This is an about page",-1),s=[t];function c(n,e){return(0,a.wg)(),(0,a.iD)("div",r,s)}var i=u(89);const l={},o=(0,i.Z)(l,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.509c5831.js.map
